e9a83697991bbb74e903f5c833433c290c1d91ab
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-01-31 12:42:06 -0700

    complete first qustion

--- objectReview.js
+++ objectReview.js
@@ -1,18 +1,21 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
 
-  //Your code here
+  var favoriteThings = {band: 'Rascal Flatts', food: 'pizza', person: 'Bobby', book: 'Meh', movie: 'Back to the Future', holiday: 'Christmas'};
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+  favoriteThings.car = "Ford";
+  favoriteThings.brand = "Prodigy";
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+  favoriteThings.food = 'Lettuce';
+  favoriteThings.book = '50 Shades of Gray';
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+  alert(favoriteThings.person);
+  alert(favoriteThings.book);
 
 
 
