8ba75fc6783d7a08de62b86b51de3f9543b69df4
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-01-31 13:28:30 -0700

    complete next 2 problems

--- callbackReview.js
+++ callbackReview.js
@@ -77,7 +77,17 @@ map(numbers, function(num){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var uniq = function(arr, cb) {
+  var uniqObj = {};
+  var uniqArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    uniqObj[arr[i]] = null;
+  }
+  for (var key in uniqObj) {
+    uniqArr.push(key);
+  }
+  cb(uniqArr);
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -90,12 +100,16 @@ uniq(names, function(uniqArr){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+var each = function(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    cb(arr[i], i);
+  }
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
