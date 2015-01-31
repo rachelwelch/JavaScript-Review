320aa936c6cbec1c5f4247d789498a6df08ee4db
Rachel Welch <rachelmsmeltzer@gmail.com>
2015-01-31 10:36:44 -0700

    complete first two questions

--- arrayReview.js
+++ arrayReview.js
@@ -2,8 +2,11 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+  var last = function(arr) {
+  	alert(arr.length);
+  }
 
+  last(threeItems);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -13,7 +16,13 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+  for(var i = 0; i < evenArray.length; i++) {
+  	if( evenArray[i] % 2 !== 0) {
+  		evenArray.splice(i, 1);
+  		i--;
+  	}
+  	console.log(evenArray);
+  }
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
